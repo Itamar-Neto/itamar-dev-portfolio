217f7404ab17b457797ffead0ab7d08e53f9facd
Itamar Henriques <itamar.henriques.rn@gmail.com>
2026-04-14 00:06:16 -0300

    feat: adiciona componente ScrollDown e ajustes na página Technology
    - Criação do componente ScrollDown com estilização CSS
    - Integração/ajustes na página Technology para uso do componente

--- itamar-portfolio/src/pages/Technology.jsx
+++ itamar-portfolio/src/pages/Technology.jsx
@@ -7,7 +7,7 @@ export default function Technology() {
                 <h2 className="text-4xl font-semi mb-4">Tecnologias e Competências</h2>
                 <p className="text-center mt-4 max-[950px]:text-justify">Acredito que compartilhar ideias e soluções acelera o aprendizado e fortalece o desenvolvimento em equipe. Estou em constante evolução como desenvolvedor, buscando aprimorar minhas habilidades técnicas diariamente e aplicar boas práticas no uso das tecnologias que domino.</p>
             </div>
-            <div id="Skills" className="flex justify-center items-center w-[70%] gap-5 flex-row flex-wrap bg-(--color-bg-containers) rounded-[45px] p-20 shadow-[0_0_20px_rgba(0,0,0,0.6)] max-[950px]:p-30">
+            <div id="Skills" className="flex flex-row flex-wrap justify-center items-center w-[70%] gap-5 bg-(--color-bg-containers) rounded-[45px] p-15 shadow-[0_0_20px_rgba(0,0,0,0.6)] max-[950px]:p-30">
                 <BallonText text="HTML5" />
                 <BallonText text="CSS3" />
                 <BallonText text="JavaScript" />
